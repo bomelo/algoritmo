43368119ab762244a26603db58bb16e9625d94f5
bomelo <binc.melo@gmail.com>
2017-01-21 20:53:38 -0200

    Exemplo de Atribuição

---
+++ ex1.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+//Atribuiçoes
+
+void main(){
+
+int a=10;
+int b=-30;
+float c;
+char d='4';
+
+c = a;
+c = a + 1.8;
+b = c;
+c = a + b;
+a = 10 + b;
+c = b * 40.5;
+
+printf("%f\n",c);
+printf("%f\n",c);
+printf("%d\n",b);
+printf("%f\n",c);
+printf("%d\n",a);
+printf("%f\n",c);
+
+}
